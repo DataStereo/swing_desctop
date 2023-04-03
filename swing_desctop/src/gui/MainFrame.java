dfa4822ca40556debc37db1a0c084c7109e1e113
oleh_m <morozov89oleg@gmail.com>
2023-04-03 12:01:59 +0300

    add a panel and a toolbar

--- swing_desctop/src/gui/MainFrame.java
+++ swing_desctop/src/gui/MainFrame.java
@@ -1,10 +1,18 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     public MainFrame(String title){
         super(title);
+
+        setLayout(new BorderLayout());
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.GREEN);
+        add(panel, BorderLayout.CENTER);
+
+
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
