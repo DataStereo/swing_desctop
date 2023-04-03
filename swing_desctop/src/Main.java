356857d70b5575b4e85091cf99dabef668de927d
oleh_m <morozov89oleg@gmail.com>
2023-04-03 09:43:26 +0300

    rename the project, add a swing custom components

--- swing_desctop/src/Main.java
+++ swing_desctop/src/Main.java
@@ -1,13 +1,11 @@
+import gui.MainFrame;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
         SwingUtilities.invokeLater(()-> {
-            JFrame mainFrame = new JFrame();
-            mainFrame.setSize(600, 400);
-            mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            mainFrame.setVisible(true);
+            new MainFrame("Test Swing app");
         });
     }
 }
---
+++ swing_desctop/src/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(String title){
+        super(title);
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
