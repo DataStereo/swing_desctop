f09037fa8029cea4b564f02078d6653eff9816e9
oleh_m <morozov89oleg@gmail.com>
2023-04-06 19:27:40 +0300

    add button clicks

--- swing_desctop/src/gui/Toolbar.java
+++ swing_desctop/src/gui/Toolbar.java
@@ -1,10 +1,32 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class RedButtonListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Red button clicked!");
+    }
+}
+
+class BlueButtonListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Blue button clicked!");
+    }
+}
 
 public class Toolbar extends JToolBar {
     public Toolbar(){
-        add(new JButton("One!"));
-        add(new JButton("Two!"));
+        final JButton redButton = new JButton("Red");
+        final JButton blueButton = new JButton("Blue");
+
+        redButton.addActionListener(new RedButtonListener());
+        blueButton.addActionListener(new BlueButtonListener());
+
+        add(redButton);
+        add(blueButton);
     }
 }
